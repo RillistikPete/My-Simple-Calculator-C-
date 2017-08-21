62edf6600e67fcf9385b64513590a5e7b442c094
Peter Forrest <peterforrest@peters-mbp.attlocal.net>
2017-08-21 12:03:27 -0500

    first commit

---
+++ Calculator.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+
+namespace Calc.Tests
+{
+    public class CalculatorShould
+    {
+        private Calculator _calculator;
+
+        public CalculatorShould()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Fact]
+        public void AddTwoIntegers()
+        {
+            // Given this input to the method
+            int sum = _calculator.Add(54, 29);
+
+            // We are asserting that the output should be this
+            Assert.Equal(sum, 83);
+        }
+
+        public static void Add(){
+            return value1 + value2;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double double1 = GetValue("What number is after 4: ");
+            double double2 = GetValue("What number is after 6: ");
+            
+            double result;
+
+            while (true)
+            {
+                Console.Write("Pick add or subtract (Type A or S)");
+                ConsoleKeyInfo info = Console.ReadKey();
+                string operation = info.Key.ToString();
+
+                switch (operation.ToUpper()) {
+                    case "A" :
+                        result = Add(double1, double2);
+                        break;
+                    case "S" :
+                        result = Subtract(double1, double2);
+                        break;
+                    default :
+                        Console.WriteLine("Choose operation");
+                        continue;
+                }
+                Console.WriteLine("\nResult: " + result);
+                Console.Read();
+                break;
+            }
+        }
+
+        private static double GetValue(string label)
+        {
+            double value;
+            while (true)
+            {
+                Console.Write(label);
+                string input = Console.ReadLine();
+                if (Double.TryParse(input, out value))
+                {
+                    return value;
+                }
+                else 
+                {
+                    Console.WriteLine("Cannot be parsed");
+                }
+
+            }
+        }
+        private static double Add(double double1, double double2)
+        {
+            return double1 + double2;
+        }
+        private static double Subtract(double double1, double double2)
+        {
+            return double1 - double2;
+        }
+    }
+}
